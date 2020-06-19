eee2383db0bda0cb13f2402b82b8f29e48df5a1c
irving <huan.guo@silot.ai>
2020-06-19 09:56:27 +0800

    first commit

---
+++ src/test/java/com/irving/shardingjdbcjemo/ShardingjdbcjemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irving.shardingjdbcjemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingjdbcjemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
